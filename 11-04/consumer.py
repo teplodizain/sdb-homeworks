019516dc4e62732e7bc16b24687395443343b9ea
Sergey Andryunin <s.andryunin@Sergeys-iMac.local>
2021-08-27 20:51:08 +0300

    fix consumer

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -11,5 +11,5 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=False)
 channel.start_consuming()
